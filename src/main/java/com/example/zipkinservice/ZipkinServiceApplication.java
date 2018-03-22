389b3fab565f8a4754f3b0895b25498ac2025d49
Fayaz Shaik <FSHAIK@amdocs.com>
2018-03-21 22:20:27 -0600

    fayaz test project

---
+++ src/main/java/com/example/zipkinservice/ZipkinServiceApplication.java
@@ -0,0 +1,20 @@
+package com.example.zipkinservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class ZipkinServiceApplication {
+
+	
+	@GetMapping("/hi")
+	String hi() {
+		return "Hello Fayaz Shaik";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinServiceApplication.class, args);
+	}
+}
