0a9ab4523f263e07cb996613232a870483f2b36e
Fayaz Shaik <FSHAIK@amdocs.com>
2018-03-22 11:24:05 -0600

    code changes

--- src/main/java/com/example/zipkinservice/ZipkinServiceApplication.java
+++ src/main/java/com/example/zipkinservice/ZipkinServiceApplication.java
@@ -4,15 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 @SpringBootApplication
 public class ZipkinServiceApplication {
+	
+	@Value("${myuser.name}")
+	private String myUserName;
 
 	
 	@GetMapping("/hi")
 	String hi() {
-		return "Hello Faiza Shaik";
+		return "Hello " + myUserName;
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(ZipkinServiceApplication.class, args);
